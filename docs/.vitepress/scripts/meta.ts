4cc3beacc4777c7b93ca634b16c73839b64a43c8
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-22 12:36:27 +0800

    更新 meta.ts

--- docs/.vitepress/scripts/meta.ts
+++ docs/.vitepress/scripts/meta.ts
@@ -1,9 +1,9 @@
 export const keywords = "Data Analysis Doc";
 export const description = "数据分析 机器学习";
 
-export const developerName = "dapan";
+export const developerName = "青石雨巷";
 
-export const title = "数据分析";
+export const title = "首页";
 
 // 更换成自己的github地址
 export const github = "https://github.com";
