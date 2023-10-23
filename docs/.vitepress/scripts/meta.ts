352869acf23a7cfc36ed2f49691014b42541f4de
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 14:30:57 +0800

    Update meta.ts

--- docs/.vitepress/scripts/meta.ts
+++ docs/.vitepress/scripts/meta.ts
@@ -6,4 +6,4 @@ export const developerName = "青石雨巷";
 export const title = "首页";
 
 // 更换成自己的github地址
-export const github = "https://github.com";
+export const github = "https://github.com/Flanderd";
