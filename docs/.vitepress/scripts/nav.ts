4ff32203b4059c5fe7dd0ea88073c03494eb57db
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 23:40:04 +0800

    更新 nav.ts

--- docs/.vitepress/scripts/nav.ts
+++ docs/.vitepress/scripts/nav.ts
@@ -6,15 +6,18 @@ const nav: DefaultTheme.NavItem[] = [
     text: "基础篇",
     items:[
       { text: "Python基础",link: "/my-blog/" },
-      { text: "统计",link: "/my-blog/" },
-      { text: "数学",link: "/my-blog/" }
+      { text: "Numpy",link: "/my-blog/" },
+      { text: "Pandas",link: "/my-blog/" },
+      { text: "Matplotlib",link: "/my-blog/" },
+      { text: "seaborn",link: "/my-blog/" }
+
     ]
   },
   { 
     text: "数据分析",
     items:[
-      { text: "Python基础",link: "/my-blog/" },
-      { text: "统计",link: "/my-blog/" },
+      { text: "三剑客",link: "/my-blog/" },
+      { text: "高数",link: "/my-blog/" },
       { text: "数学",link: "/my-blog/" }
     ]
   },
