bcf5d47afa3037646c429998deaf55ae38a1f8cb
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 19:36:01 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -46,6 +46,22 @@ export default defineConfig({
       pattern: `${github}/blob/master/docs/:path`,
       text: "为此页提供修改建议",
     },
-    socialLinks: [{ icon: "github", link: github }],
+    socialLinks: [
+      {
+        icon: {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-bilibili" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 10a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v6a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4v-6z" />
+  <path d="M8 3l2 3" />
+  <path d="M16 3l-2 3" />
+  <path d="M9 13v-2" />
+  <path d="M15 11v2" />
+</svg>'
+        },
+        link: 'https://space.bilibili.com/1619011071/',
+        // You can include a custom label for accessibility too (optional but recommended):
+        ariaLabel: 'cool link'
+      },
+      { icon: "github", link: github }],
   },
 });
