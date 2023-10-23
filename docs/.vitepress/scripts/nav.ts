835ee02ef9fcf13b49279d93521543c419050c62
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 19:31:04 +0800

    Update nav.ts

--- docs/.vitepress/scripts/nav.ts
+++ docs/.vitepress/scripts/nav.ts
@@ -27,20 +27,4 @@ const nav: DefaultTheme.NavItem[] = [
   },
   { text: "Github",link: "/my-blog/" },
 ];
-export default {
-  themeConfig: {
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
-      { icon: 'twitter', link: '...' },
-      {
-        icon: {
-          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
-        },
-        link: '...',
-        // You can include a custom label for accessibility too (optional but recommended):
-        ariaLabel: 'cool link'
-      }
-    ]
-  }
-}
 export default nav;
