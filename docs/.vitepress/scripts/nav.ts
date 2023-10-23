22c5a05eeb4fa431401af9b862f7c222263c86cd
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 18:56:12 +0800

    Update nav.ts

--- docs/.vitepress/scripts/nav.ts
+++ docs/.vitepress/scripts/nav.ts
@@ -2,9 +2,16 @@ import type { DefaultTheme } from "vitepress/types";
 
 // 菜单
 const nav: DefaultTheme.NavItem[] = [
-  { text: "基础篇", link: "/my-blog/" },
-  { text: "数据分析", link: "/my-blog/" },
-  { text: "机器学习", link: "/my-blog/" },
-  { text: "Github", link: "/my-blog/" },
+  { 
+    text: "基础篇",
+    items:[
+       { text: "Python基础"， link: "/my-blog/" }，
+       { text: "统计"， link: "/my-blog/" }，
+       { text: "数学"， link: "/my-blog/" }，
+  
+]},
+  { text: "数据分析"， link: "/my-blog/" }，
+  { text: "机器学习"， link: "/my-blog/" },
+  { text: "Github"， link: "/my-blog/" },
 ];
 export default nav;
