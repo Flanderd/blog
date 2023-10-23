3f3c14fce89c76425c6a6dd29e60a73a75f6128f
Durian <112143058+Flanderd@users.noreply.github.com>
2023-10-23 14:21:12 +0800

    Update sidebar.ts

--- docs/.vitepress/scripts/sidebar.ts
+++ docs/.vitepress/scripts/sidebar.ts
@@ -11,4 +11,12 @@ export default {
         { text: "数据清洗", link: "/my-blog/数据清洗" },      ],
     },
   ],
-} as DefaultTheme.Sidebar;
+   "/my-blog": [
+    {
+      text: "基础",
+      items: [
+        { text: "介绍", link: "/my-blog/" },
+        { text: "学习心得", link: "/my-blog/study" },
+        { text: "数据清洗", link: "/my-blog/数据清洗" },      ],
+    },
+  ],} as DefaultTheme.Sidebar;
