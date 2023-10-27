6fedb3ec92da3842b366bb3190d8c48f59fa9d3f
青石雨巷 <112143058+Flanderd@users.noreply.github.com>
2023-10-27 16:35:04 +0800

    Update sidebar.ts

--- docs/.vitepress/scripts/sidebar.ts
+++ docs/.vitepress/scripts/sidebar.ts
@@ -8,7 +8,8 @@ export default {
       items: [
         { text: "介绍", link: "/my-blog/" },
         { text: "学习心得", link: "/my-blog/study" },
-        { text: "MathType试用期3万天!", link: "/my-blog/MathType试用期3万天！.md" },      ],
+        { text: "MathType试用期3万天!", link: "/my-blog/MathType试用期3万天！.md" }，
+        { text: "MathType试用期3万天!", link: "/my-blog/语雀.md" },],
     },
   ],
   "/data": [
